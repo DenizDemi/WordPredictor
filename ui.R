7ed5e4af79570d205143461fe46d1eb1b9e2abf9
Deniz Demiroglu <140518167+DenizDemi@users.noreply.github.com>
2023-12-24 06:35:51 -0500

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,42 @@
+#'
+#' Next Word Predictor
+#'  
+#' This is a very simple application for predicting next word based on user input
+#' 
+#' 
+#' @author Deniz
+#' 
+#' @details This app takes in the text input from user and predicts next word
+#'  
+#' @import shiny
+
+
+library(shiny)
+
+# Define UI for application that predicts next word
+fluidPage(
+
+    # Application title
+    titlePanel("Next Word Predictor"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+              h3("Instructions"),
+              tags$div(class="header", checked=NA,
+                       tags$p("Type in the text field below and click \"Submit\" button or press \"Enter\", to see the most likely next word 
+                        displayed at the main panel. ")),
+              h3("Input"),
+              textInput("inputWords",
+                        "Please type here:"),
+              submitButton("Submit")
+  
+        ),
+
+        # print the predicted value
+        mainPanel(
+              h3("Predicted next word:"),
+              textOutput("value")
+        )
+    )
+)
